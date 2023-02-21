262786a213b127ef81a70ce9fa6349f33f559c0f
binyusuf <binyusuf30@gmail.com>
2023-02-21 03:56:49 -0800

    updated

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+	/**
+	 * main - main function
+	 * Description: prints the alphabets _putchar
+	 * Return: Always 0
+	 *
+	 */
+
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+
+	return (0);
+
+}
