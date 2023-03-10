6fd07d5aca60f3590214bf5c3a36cae6ca89a280
binyusuf <binyusuf30@gmail.com>
2023-03-10 09:06:54 -0800

    updated

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -9,15 +9,15 @@
 
 int isInteger(const char *s)
 {
-	int i = 0;
-	while (s[i] != '\0')
-	{
-		if (s[i] < '0' || s[i] > '9')
+int i = 0;
+while (s[i] != '\0')
+{
+	if (s[i] < '0' || s[i] > '9')
 		return (0);
-		i++;
-	}
-		return (1);
-	}
+	i++;
+}
+return (1);
+}
 
 /**
  * main - adds positive numbers
@@ -28,32 +28,32 @@ int isInteger(const char *s)
 
 int main(int argc, char const *argv[])
 {
-	int i = 0, coinUsed = 0, coin = 0;
-	int coins[] = {25, 10, 5, 2, 1};
+int i = 0, coinUsed = 0, coin = 0;
+int coins[] = {25, 10, 5, 2, 1};
 
-	if (argc != 2)
-	{
-		printf("Error\n");
-		return (1);
-	}
-	if (isInteger(argv[1]))
-	{
+if (argc != 2)
+{
+	printf("Error\n");
+	return (1);
+}
+if (isInteger(argv[1]))
+{
 	i = atoi(argv[1]);
 	while (i > 0 && coin <= 4)
+	{
+		if (i >= coins[coin])
 		{
-			if (i >= coins[coin])
-			{
-				i -= coins[coin];
-				coinUsed++;
-			}
-			else
-			{
-				coin++;
-			}
+			i -= coins[coin];
+			coinUsed++;
+		}
+		else
+		{
+			coin++;
 		}
 	}
+}
 
-	printf("%i\n", coinUsed);
+printf("%i\n", coinUsed);
 
-	return (0);
+return (0);
 }
